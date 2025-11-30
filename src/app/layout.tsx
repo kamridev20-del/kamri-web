03c8b0717fe5f5c0f0a49bfb9881bffdec30d525
KevDev1437 <projectskevin834@gmail.com>
2025-11-30 14:34:56 +0100

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'KAMRI - Hub',
+  title: 'KAMRIᵤᵤᵤ',
   description: 'Shop-Buy-Go',
 }
 
